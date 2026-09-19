df8e139e3896969ab36d5f4b7a57684df1248ed5
Arkadiy Derevyan <maxfnine@gmail.com>
2026-09-19 20:40:50 +0300

    Fixed bug where placeholder was seen as text

--- ToDoList/NewTaskModalView.swift
+++ ToDoList/NewTaskModalView.swift
@@ -84,7 +84,9 @@ class NewTaskModalView: UIView {
     }
 
     @IBAction func submitButtonTapped(_ sender: UIButton) {
-        guard let caption = descriptionTextView.text,caption.count>=4 else {
+        guard let caption = descriptionTextView.text,
+              descriptionTextView.textColor != UIColor.placeholderText,
+                caption.count>=4 else {
             return
         }
         let selectedRow = categoryPickerView.selectedRow(inComponent: 0)
